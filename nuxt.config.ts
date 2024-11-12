86ea8d75e8843f895c67e70a61874d1c9f8eb1d5
rhmkstk <rahmi.kstk@gmail.com>
2024-11-12 13:33:20 +0300

    remove unused lines

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import path from 'path';
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  alias: {
-    '#layer': path.resolve(__dirname), // Fully resolved path to the layer root
-  },
   modules:['@lui-ui/lui-nuxt', '@nuxtjs/tailwindcss'],
   tailwindcss:{
     configPath: './tailwind.config'
