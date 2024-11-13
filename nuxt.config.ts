b9292e9504cf58760aac206b4603246c31a39f53
rhmkstk <rahmi.kstk@gmail.com>
2024-11-13 11:53:32 +0300

    change module order

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules:['@lui-ui/lui-nuxt', '@nuxtjs/tailwindcss'],
-  tailwindcss:{
-    configPath: './tailwind.config'
-  }
-})
+  modules: ["@nuxtjs/tailwindcss", "@lui-ui/lui-nuxt"],
+  tailwindcss: {
+    configPath: "./tailwind.config",
+  },
+});
