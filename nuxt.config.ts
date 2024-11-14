082da4cba35377d5cf11c915d2045696bc83d85d
rhmkstk <rahmi.kstk@gmail.com>
2024-11-14 12:20:40 +0300

    add lui-vue as dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "./modules/lui-nuxt-test"],
+  modules: ["@nuxtjs/tailwindcss", "@lui-ui/lui-nuxt"],
   tailwindcss: {
     configPath: "./tailwind.config",
+  },
+  vite: {
+    optimizeDeps: {
+      include: ['@lui-ui/lui-vue'],
+    },
   },
 });
